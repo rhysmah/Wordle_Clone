8ee1d04a134a89510624fdcf8a6fe27f49ad489a
rhysmah <97256979+rhysmah@users.noreply.github.com>
2022-12-24 15:14:55 -0800

    Various updates

--- src/main/java/com/example/wordle_clone/MainMenu.java
+++ src/main/java/com/example/wordle_clone/MainMenu.java
@@ -14,10 +14,21 @@ import java.io.IOException;
  */
 public class MainMenu extends Application {
 
+    private static final int MAIN_MENU_WIDTH  = 320;
+    private static final int MAIN_MENU_HEIGHT = 240;
+
+    /**
+     * Starts the main menu.
+     * @param stage the primary stage for this application, onto which
+     * the application scene can be set.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages.
+     * @throws IOException if main-menu.fxml cannot be found.
+     */
     @Override
     public void start(final Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainMenu.class.getResource("/main-menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), MAIN_MENU_WIDTH, MAIN_MENU_HEIGHT);
         stage.setTitle("Wordle Clone");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/wordle_clone/Player.java
+++ src/main/java/com/example/wordle_clone/Player.java
@@ -3,7 +3,7 @@ package com.example.wordle_clone;
 /**
  * Represents a Wordle Player.
  * @author Mahannah
- * @version 17-12-22
+ * @version 24-12-22
  */
 public final class Player {
 
@@ -12,34 +12,15 @@ public final class Player {
      */
     public static final int MAX_NUMBER_PLAYER_TURNS = 6;
 
-    private static final String  INITIAL_GUESS_WORD_VALUE  = "";
-    private static final int     INITIAL_PLAYER_TURN_VALUE = 0;
+    private static final int INITIAL_PLAYER_TURN_VALUE = 0;
 
-    private String guessWord;
-    private int    playerTurn;
+    private int playerTurn;
 
     /**
      * Creates an object of type Player.
      */
     public Player() {
         this.playerTurn = INITIAL_PLAYER_TURN_VALUE;
-        this.guessWord  = INITIAL_GUESS_WORD_VALUE;
-    }
-
-    /**
-     * Sets the guess word.
-     * @param newGuessWord the new word (String)
-     */
-    public void setGuessWord(final String newGuessWord) {
-        this.guessWord = newGuessWord;
-    }
-
-    /**
-     * Returns user input.
-     * @return user word in all uppercase (String).
-     */
-    public String getGuessWord() {
-        return guessWord;
     }
 
     /**
